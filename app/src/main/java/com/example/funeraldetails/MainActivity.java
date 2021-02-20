bcede249f9461efa5ea0fc666f30bc224cd1e718
Sudarshan Vittal <sudarshanvittal@gmail.com>
2021-02-20 10:46:07 +0200

    App signed

--- app/src/main/java/com/example/funeraldetails/MainActivity.java
+++ app/src/main/java/com/example/funeraldetails/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.funeraldetails;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
@@ -14,8 +13,13 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -27,11 +31,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText txtEditDate, txtEditTime;
     private TextView txtDetails;
     private Button btnDetails, btnReturn;
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
         txtEditDate = this.findViewById(R.id.txtEditDate);
         txtEditTime = this.findViewById(R.id.txtEditTime);
